0becf3a4377e6b3ced9a1fc6272cfaf39d06b9ac
Aquele_Cara <122459409+Igor-CA@users.noreply.github.com>
2023-11-12 02:18:27 -0300

    Image manipulation function

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
 	const canvasRef = useRef(null);
 	const contextRef = useRef(null);
+	const [rangeValues, setRangeValues] = useState({ zoom: 1, x: 50, y: 50 });
+	const [imageFile, setImageFile] = useState(null);
 	useEffect(() => {
 		const canvas = canvasRef.current;
 		const context = canvas.getContext("2d");
 		contextRef.current = context;
 	});
 
-
-
-	const handleChange = (event) => {
-		
+	const handleFileInputChange = (event) => {
 		const file = event.target.files[0];
 		const imageType = /image.*/;
 
@@ -23,38 +22,64 @@ function App() {
 		reader.onload = (e) => {
 			const imageFile = new Image();
 			imageFile.onload = function () {
-				processImage(imageFile);
+				setImageFile(imageFile);
+				renderImage(imageFile);
 			};
 			imageFile.src = e.target.result;
 		};
 		reader.readAsDataURL(file);
 	};
 
-	const processImage = (img) => {
-		const canvas = canvasRef.current;
-		const context = contextRef.current;
-		
-		canvas.width = img.width;
-		canvas.height = img.height;
-		context.drawImage(img, 0, 0, img.width, img.height);
+	const handleRadioInputChange = (e) => {
+		const { name, value } = e.target;
+		setRangeValues((previos) => {
+			return { ...previos, [name]: value };
+		});
+		renderImage(imageFile);
+	};
+	const renderImage = (img) => {
+		const croppedImage = cropImage(img);
+		const croppedImageData = croppedImage.data.slice()
+		const grayScaleImage = applyGrayScaleFilter(croppedImageData);
 
-		const scannedImage = context.getImageData(
+		const scannedImageCopy = new ImageData(
+			croppedImage.width,
+			croppedImage.height
+		);
+
+		scannedImageCopy.data.set(grayScaleImage);
+		contextRef.current.putImageData(scannedImageCopy, 0, 0);
+	};
+	const cropImage = (image) => {
+		const canvas = canvasRef.current;
+
+		const { zoom, x, y } = rangeValues;
+		const imageSize = Math.min(image.width, image.height) * zoom;
+
+		const selectedX = (x * (image.width - imageSize)) / 100;
+		const selectedY = (y * (image.height - imageSize)) / 100;
+		const selectedWidth = imageSize;
+		const selectedHeight = imageSize;
+
+		const croppedCanvas = document.createElement("canvas");
+		croppedCanvas.width = canvas.width;
+		croppedCanvas.height = canvas.height;
+		const croppedContext = croppedCanvas.getContext("2d");
+		croppedContext.drawImage(
+			image,
+			selectedX,
+			selectedY,
+			selectedWidth,
+			selectedHeight,
 			0,
 			0,
 			canvas.width,
 			canvas.height
 		);
-		const imageData = scannedImage.data.slice();
-		const grayScaleImage = applyGrayScaleFilter (imageData);
 
-		const scannedImageCopy = new ImageData(
-			scannedImage.width,
-			scannedImage.height
-		);
-		scannedImageCopy.data.set(grayScaleImage);
-		contextRef.current.putImageData(scannedImageCopy, 0, 0);
-	}
-	const applyGrayScaleFilter  = (imageData) => {
+		return croppedContext.getImageData(0, 0, canvas.width, canvas.height);
+	};
+	const applyGrayScaleFilter = (imageData) => {
 		for (let i = 0; i < imageData.length; i += 4) {
 			const grayScale =
 				imageData[i] * 0.299 + //Red brightness
@@ -65,8 +90,8 @@ function App() {
 			imageData[i + 1] = grayScale;
 			imageData[i + 2] = grayScale;
 		}
-		return imageData
-	}
+		return imageData;
+	};
 
 	return (
 		<div className="App">
@@ -74,14 +99,46 @@ function App() {
 				type="file"
 				name="fileInput"
 				id="fileInput"
-				onChange={handleChange}
+				onChange={handleFileInputChange}
 			/>
 			<canvas
+				id="imageCanvas"
 				width="400"
 				height="400"
-				id="imageCanvas"
 				ref={canvasRef}
 			></canvas>
+			{imageFile && (
+				<>
+					<label htmlFor="zoom">Zoom</label>
+					<input
+						type="range"
+						min={0.1}
+						max={1}
+						step={0.01}
+						defaultValue={1}
+						name="zoom"
+						onChange={handleRadioInputChange}
+					/>
+					<label htmlFor="x">X</label>
+					<input
+						type="range"
+						min={0}
+						max={100}
+						defaultValue={50}
+						name="x"
+						onChange={handleRadioInputChange}
+					/>
+					<label htmlFor="y"></label>Y
+					<input
+						type="range"
+						min={0}
+						max={100}
+						defaultValue={50}
+						name="y"
+						onChange={handleRadioInputChange}
+					/>
+				</>
+			)}
 		</div>
 	);
 }
