cad49f7f8cbe55aed2c538279c6e38644b3060fb
Aquele_Cara <122459409+Igor-CA@users.noreply.github.com>
2023-12-29 17:06:05 -0300

    Optimization and cleanup

--- src/algorithm.js
+++ src/algorithm.js
@@ -1,6 +1,5 @@
-
 //Variation of Bresenham's Algorithm
-function calculateLine(x1, y1, x2, y2) {
+export function calculateLine(x1, y1, x2, y2) {
 	const incX = Math.sign(x2 - x1);
 	const dX = Math.abs(x2 - x1);
 
@@ -8,17 +7,17 @@ function calculateLine(x1, y1, x2, y2) {
 	const dY = Math.abs(y2 - y1);
 
 	const XaY = dX > dY;
-	const incD = -2 * Math.abs(dX - dY); 
+	const incD = -2 * Math.abs(dX - dY);
 	const incS = 2 * Math.min(dX, dY);
 	let cmpt = Math.max(dX, dY);
 
 	let err = incD + cmpt;
 	let X = x1;
 	let Y = y1;
-	const pixelList = []
-	while (cmpt >= 0) { 
-		pixelList.push([X,Y])
-		cmpt -= 1; 
+	const pixelList = [];
+	while (cmpt >= 0) {
+		pixelList.push([X, Y]);
+		cmpt -= 1;
 
 		if (err >= 0 || XaY) {
 			X += incX;
@@ -36,101 +35,137 @@ function calculateLine(x1, y1, x2, y2) {
 	return pixelList;
 }
 
-//Make matrix 4x smaller since only one value is important for each pixel (RGB are equal and all pixels have alpha of same value) 
+//Make matrix 4x smaller since only one value is important for each pixel (RGB are equal and all pixels have alpha of same value)
 function reduceMatrix(pixelMatrix) {
 	const reducedMatrix = [];
 	for (let i = 0; i < pixelMatrix.length; i += 4) {
 		reducedMatrix.push(pixelMatrix[i]);
 	}
-	return reducedMatrix
+	return reducedMatrix;
 }
 
-//Turn the matrix into a two dimmensional array instead of one 
-function simplifyMatrix(pixelMatrix, width, heigh){
+//Turn the matrix into a two dimmensional array instead of one
+function simplifyMatrix(pixelMatrix, width, heigh) {
 	const simplifiedMatrix = [];
-	let column = [];
 	for (let y = 0; y < heigh; y++) {
+		const column = [];
 		for (let x = 0; x < width; x++) {
 			const index = x * width + y;
 			column.push(pixelMatrix[index]);
 		}
 		simplifiedMatrix.push(column);
-		column = [];
 	}
 	return simplifiedMatrix;
-
 }
 
 function measureBrightness(imageData, line) {
-	const imageLine = [];
+	let lineBrightness = 0;
 	for (let pixel of line) {
 		const x = pixel[0];
 		const y = pixel[1];
-		const imagePixel = imageData[x][y];
-		imageLine.push(imagePixel);
+		const pixelBrightnes = imageData[x][y];
+		lineBrightness += pixelBrightnes;
 	}
-	const brightness = imageLine.reduce((a, b) => a + b)/imageLine.length;
+	const averageBrithness = lineBrightness / line.length;
 
-	return brightness;
+	return averageBrithness;
 }
 
-function geneatePinCoodinates(numberOfPoints, canvasSize){
+function geneatePinCoodinates(numberOfPoints, canvasSize) {
 	const center = [canvasSize / 2, canvasSize / 2];
 	const coords = [];
 	for (let i = 0; i < numberOfPoints; i++) {
-		const x =
-			Math.floor((center[0] + (Math.cos((i * 2 * Math.PI) / numberOfPoints) * (canvasSize-1)) / 2))
-		const y =
-			Math.floor((center[1] + (Math.sin((i * 2 * Math.PI) / numberOfPoints) * (canvasSize-1)) / 2))
+		const x = Math.floor(
+			center[0] +
+				(Math.cos((i * 2 * Math.PI) / numberOfPoints) * (canvasSize - 1)) / 2
+		);
+		const y = Math.floor(
+			center[1] +
+				(Math.sin((i * 2 * Math.PI) / numberOfPoints) * (canvasSize - 1)) / 2
+		);
 		coords.push([x, y]);
 	}
 	return coords;
 }
 
+function preCalculateLines(numberOfNails, canvasSize) {
+	const lines = {};
+	const nailsCoords = geneatePinCoodinates(numberOfNails, canvasSize);
+	for (let nailOne = 0; nailOne < numberOfNails; nailOne++) {
+		for (let nailTwo = nailOne + 1; nailTwo < numberOfNails; nailTwo++) {
+			const x1 = nailsCoords[nailOne][0];
+			const y1 = nailsCoords[nailOne][1];
+			const x2 = nailsCoords[nailTwo][0];
+			const y2 = nailsCoords[nailTwo][1];
+			const line = calculateLine(x1, y1, x2, y2);
 
-export default function generateStringArt(imageData, numberOfThreads, numberOfNails, canvasSize, lineTranparency){
-	const reduced = reduceMatrix(imageData)
-	const simplified = simplifyMatrix(reduced, canvasSize, canvasSize)
-	const imageCopy = simplified
-	const steps = [0]
-	const nailsCoords = geneatePinCoodinates(numberOfNails, canvasSize)
-	let nail = 0
-	for(let i=0; i<numberOfThreads; i++){
-		let darkestValue = Infinity
-		let nextNail = 0
-		for(let secondNail = 0; secondNail<numberOfNails; secondNail++){
-			if (nail === secondNail) continue
-			const x1 = nailsCoords[nail][0]
-			const y1 = nailsCoords[nail][1]
-			const x2 = nailsCoords[secondNail][0]
-			const y2 = nailsCoords[secondNail][1]
-			const line = calculateLine(x1,y1,x2,y2)
-			const lineBrightness = measureBrightness(imageCopy, line)
-			
-			if(lineBrightness < darkestValue){
-				darkestValue = lineBrightness
-				nextNail = secondNail
-			}
+			const key = `${nailOne}-${nailTwo}`;
+			lines[key] = line;
 		}
-		
-		
-		const x1 = nailsCoords[nail][0]
-		const y1 = nailsCoords[nail][1]
-		const x2 = nailsCoords[nextNail][0]
-		const y2 = nailsCoords[nextNail][1]	
-		const removedLine = calculateLine(x1,y1,x2,y2)
-		removedLine.forEach(pixel => {
-			const x = pixel[0]
-			const y = pixel[1]
-			imageCopy[x][y] += Math.floor(lineTranparency * 255)
-		})
-		
+	}
+	return lines;
+}
 
+//Find the darkest line and the finishing nail of that line from a giving starting nail
+function chooseDarkestLine(image, numberOfNails, lines, startingNail) {
+	let darkestValue = Infinity;
+	let finishingNail = 0;
+	for (let secondNail = 0; secondNail < numberOfNails; secondNail++) {
+		if (startingNail === secondNail) continue;
+		const nailOne = Math.min(startingNail, secondNail);
+		const nailTwo = Math.max(startingNail, secondNail);
+		const key = `${nailOne}-${nailTwo}`;
+		const line = lines[key];
+		const lineBrightness = measureBrightness(image, line);
 
-		steps.push(nextNail)
-		nail = nextNail
-
+		if (lineBrightness < darkestValue) {
+			darkestValue = lineBrightness;
+			finishingNail = secondNail;
+		}
 	}
 
-	return steps
+	const nailOne = Math.min(startingNail, finishingNail);
+	const nailTwo = Math.max(startingNail, finishingNail);
+	const key = `${nailOne}-${nailTwo}`;
+
+	return [finishingNail, lines[key]];
+}
+
+function removeLineFromImage(line, image, lineTranparency) {
+	line.forEach((pixel) => {
+		const x = pixel[0];
+		const y = pixel[1];
+		image[x][y] += Math.floor(lineTranparency * 255);
+	});
+	return image;
+}
+
+export default async function generateStringArt(
+	imageData,
+	numberOfThreads,
+	numberOfNails,
+	canvasSize,
+	lineTranparency
+) {
+	const reduced = reduceMatrix(imageData);
+	const simplified = simplifyMatrix(reduced, canvasSize, canvasSize);
+	const lines = preCalculateLines(numberOfNails, canvasSize);
+	const steps = [0];
+
+	let imageCopy = simplified;
+
+	let nail = 0;
+	for (let i = 0; i < numberOfThreads; i++) {
+		const [nextNail, darkestLine] = chooseDarkestLine(
+			imageCopy,
+			numberOfNails,
+			lines,
+			nail
+		);
+		imageCopy = removeLineFromImage(darkestLine, imageCopy, lineTranparency);
+		steps.push(nextNail);
+		nail = nextNail;
+	}
+
+	return steps;
 }
