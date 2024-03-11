afcc7c8cc76b962cdd8eecaacc10df89e803434f
Igor Caldeira Andrade <122459409+Igor-CA@users.noreply.github.com>
2024-03-11 19:31:22 -0300

    Revert "Redesign and cleanup"

--- src/worker.js
+++
@@ -1,7 +0,0 @@
-import generateStringArt from "./algorithm";
-
-onmessage = (e) => {
-	const {imageData, numberOfPoints, numberOfThreads, screenSize, lineTranparency} = e.data
-	const generatedSteps = generateStringArt(imageData, numberOfThreads, numberOfPoints, screenSize, lineTranparency)
-	postMessage(generatedSteps)
-};
